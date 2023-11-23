a124db541b43c7022c5b4c9ded70b703884aa384
unko <110211701+ShimadaNanaki@users.noreply.github.com>
2023-11-23 17:36:56 +0900

    コミットメッセージ

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/product-frontend/' : '/',
-  outputDir: 'docs',
   plugins: [
     vue(),
   ],
